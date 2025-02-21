cf42fe00b589730be54e6ab32db276f845a0884c
Alisa Wu <areuuie@gmail.com>
2025-02-21 16:55:01 -0500

    [64] [Time Beats: 66.57%] [Memory Beats: 15.17%] - LeetPush

--- 64-Minimum-Path-Sum.js
+++ 64-Minimum-Path-Sum.js
@@ -3,31 +3,27 @@
  * @return {number}
  */
 var minPathSum = function(grid) {
-    function minSum(i, j) {
-        if (i === 0 && j === 0) {
-            return grid[i][j];
-        }
-
-        if (dp[i][j] !== -1) {
-            return dp[i][j];
-        }
-        
-        let left = Infinity;
-        let up = Infinity;
-
-        if (j > 0) left = grid[i][j] + minSum(i,j-1);
-        if (i > 0) up = grid[i][j] + minSum(i-1,j);
-
-        dp[i][j] = Math.min(left, up);
-        return dp[i][j];
-    }
-
     let n = grid.length;
     let m = grid[0].length;
     
-    let dp = new Array(n).fill(-1).map(() => {
-       return new Array(m).fill(-1)
-    });
+    let prev = new Array(m).fill(-1);
 
-    return minSum(n-1, m-1);
+    for(let i = 0; i < n; i++) {
+        let curr = new Array(m).fill(-1);
+        for(let j = 0;j < m; j++) {
+            if (i === 0 && j === 0) {
+                curr[j] = grid[i][j];
+            } else {
+                let left = Infinity;
+                let up = Infinity;
+
+                if (j > 0) left = grid[i][j] + curr[j-1];
+                if (i > 0) up = grid[i][j] + prev[j];
+
+                curr[j] = Math.min(left, up);
+            }
+        }
+        prev = curr;
+    }
+    return prev[m-1];
 };
