ce15e05d9589218d4cb30aa78392c9821f1aee0b
Alisa Wu <areuuie@gmail.com>
2025-02-25 23:19:03 -0500

    [217] [Time Beats: undefined] [Memory Beats: undefined] - LeetPush

--- 217-Contains-Duplicate.py
+++ 217-Contains-Duplicate.py
@@ -1,12 +1,9 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        ans = dict()
-        for i in nums:
-            if i not in ans:
-                ans[i] = 1
-            else: 
-                ans[i] += 1
-            if ans[i] == 2:
+        freq = {}
+
+        for num in nums:
+            freq[num] = freq.get(num, 0) + 1
+            if freq[num] >= 2:
                 return True
         return False
-        
